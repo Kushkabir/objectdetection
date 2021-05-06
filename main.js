1b9e1951fda84eb0258a14ba76cccc40a43cb4c2
Kush Kabir Mehra <72560381+Kushkabir@users.noreply.github.com>
2021-05-06 17:39:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+function tvac(){
+    window.location="tvac.html";
+}
+function desc(){
+    window.location="desc.html";
+}
+function bottles(){
+    window.location="bottles.html";
+}
+function fruitbasket(){
+    window.location="fruitbasket.html";
+}
